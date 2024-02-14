95811e2d10e44f433f5341fa3433d3be8d519b32
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-02-14 17:57:16 +0400

    third commit

---
+++ tasks.dart
@@ -0,0 +1,10 @@
+// ad, soyad, yas, dogum tarixi(il) qeyd edin ve ekranda cixardin . QEYD: "dogum tarixi" dynamic , "yas" double olmalidir
+// "Ad"-daki butun elementleri boyuk herflerle ekrana cixardin, "Soyad"-daki butun elementleri kicik herfle ekrana cixardin
+// "Ad" - daki elementleri deyise bilecek metod isledin; 
+// ad ve soyaddakı elementlerin  uzunluguna bir integer deyiseni verin ve ekrana bu deyiseni cixardin
+// 3 ferqli List yaradin, onlarin tiplerini bildirin , 1.integer type 2.String type 3. hem integer , hem String
+
+//key = "string" , value = "List<String>" olan Map yaradin. 1. istifadeciler  2. istifadecilerin yaslari 3. vezifeleri 
+//qeyd: her listde sadece 3 item olmasi kifayetdir. 
+
+
